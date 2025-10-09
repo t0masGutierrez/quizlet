0bf6d28f953be7d3c3db5dcd69cade0c27095063
Tomas Gutierrez <tgut03@gmail.com>
2025-10-09 16:10:03 -0500

    add main()

--- quizlet.py
+++ quizlet.py
@@ -1,36 +1,45 @@
 import os
 
-base = os.path.expanduser("~") + "/Obsidian/brainTwo/"
-output = os.path.expanduser("~") + "/Downloads/Obsidian/"
 
-for dir in os.listdir(base): # loop through folders inside obsidian
-    if not dir.startswith(".") and dir not in ["Templates", "Linguistics"]:
-        folder_name = dir
-        folder_path = base + folder_name
+def md_to_txt():
+    base = os.path.expanduser("~") + "/Obsidian/brainTwo/"
+    output = os.path.expanduser("~") + "/Downloads/Obsidian/"
 
-        for file in os.listdir(folder_path): # loop through files inside obsidian folder
-            if file.endswith(".md"):
-                file_path = folder_path + "/" + file
-                file_name = os.path.basename(file)
-                file_name = file_name.removesuffix(".md")
+    for dir in os.listdir(base): # loop through folders inside obsidian
+        ignore = ["Templates", "Linguistics"]
+        if not dir.startswith(".") and dir not in ignore:
+            folder_name = dir
+            folder_path = base + folder_name
 
-                with open(file_path, "r") as md: # read from .md files
-                    data = md.read()
-                    os.makedirs(output + folder_name + "/", exist_ok=True)
+            for file in os.listdir(folder_path): # loop through files inside obsidian folder
+                if file.endswith(".md"):
+                    file_path = folder_path + "/" + file
+                    file_name = os.path.basename(file)
+                    file_name = file_name.removesuffix(".md")
 
-                    with open(output + folder_name + "/" + file_name + ".txt", "w") as txt: # write to .txt files
-                        txt.write(data)
+                    with open(file_path, "r") as md: # read from .md files
+                        data = md.read()
+                        os.makedirs(output + folder_name + "/", exist_ok=True)
 
-                    with open(output + folder_name + "/" + file_name + ".txt", "r") as txt: # read from .txt files
-                        lines = txt.readlines()
-                        len_lines = len(lines[:-2])
-
-                        for i in range(len_lines): # loop through lines inside .txt file
-
-                            pattern = not lines[i].startswith("- ") and lines[i+1].startswith("- ")
-                            pattern1 = lines[i+1].startswith("$$") and not lines[i+2].startswith("---")
-                            if pattern or pattern1: # add newlines between term and definition
-                                lines[i] += "\n"
-                        
                         with open(output + folder_name + "/" + file_name + ".txt", "w") as txt: # write to .txt files
-                            txt.writelines(lines)
+                            txt.write(data)
+
+                        with open(output + folder_name + "/" + file_name + ".txt", "r") as txt: # read from .txt files
+                            lines = txt.readlines()
+                            len_lines = len(lines[:-2])
+
+                            for i in range(len_lines): # loop through lines inside .txt file
+
+                                pattern = not lines[i].startswith("- ") and lines[i+1].startswith("- ")
+                                pattern1 = lines[i+1].startswith("$$") and not lines[i+2].startswith("---")
+                                if pattern or pattern1: # add newlines between term and definition
+                                    lines[i] += "\n"
+                            
+                            with open(output + folder_name + "/" + file_name + ".txt", "w") as txt: # write to .txt files
+                                txt.writelines(lines)
+    return None
+
+def main():
+    md_to_txt()
+
+main()
