94389ebb689b0a3970e6f82e387b0d16e16b8222
Tomas Gutierrez <tgut03@gmail.com>
2025-10-17 11:02:11 -0500

    move arguments inside main

--- quizlet.py
+++ quizlet.py
@@ -1,37 +1,42 @@
 import os
 
 
-def md_to_txt():
-    input = os.path.expanduser("~") + "/Obsidian/brainTwo/"
-    output = os.path.expanduser("~") + "/Downloads/Obsidian/"
-
-    for dir in os.listdir(input): # loop through folders inside obsidian
+def md_to_txt(input, output):
+    # loop through folders inside obsidian
+    for dir in os.listdir(input):
         ignore = ["Templates", "Linguistics"]
         if not dir.startswith(".") and dir not in ignore:
             folder_name = dir
             folder_path = input + folder_name
 
-            for file in os.listdir(folder_path): # loop through files inside obsidian folder
+            # loop through files inside obsidian folder
+            for file in os.listdir(folder_path):
                 if file.endswith(".md"):
                     file_path = folder_path + "/" + file
                     file_name = os.path.basename(file)
                     file_name = file_name.removesuffix(".md")
 
-                    with open(file_path, "r") as md: # read from .md files
+                    # read from .md files
+                    with open(file_path, "r") as md:
                         data = md.read()
                         os.makedirs(output + folder_name + "/", exist_ok=True)
 
-                        with open(output + folder_name + "/" + file_name + ".txt", "w") as txt: # write to .txt files
+                        # write to .txt files
+                        with open(output + folder_name + "/" + file_name + ".txt", "w") as txt:
                             txt.write(data)
 
-                        with open(output + folder_name + "/" + file_name + ".txt", "r") as txt: # read from .txt files
+                        # read from .txt files
+                        with open(output + folder_name + "/" + file_name + ".txt", "r") as txt:
                             lines = txt.readlines()
                             len_lines = len(lines[:-2])
 
-                            for i in range(len_lines): # loop through lines inside .txt file
+                            # loop through lines inside .txt file
+                            for i in range(len_lines):
                                 pattern = not lines[i].startswith("- ") and lines[i+1].startswith("- ")
                                 pattern1 = lines[i+1].startswith("$$") and not lines[i+2].startswith("---")
-                                if pattern or pattern1: # add newlines between term and definition
+
+                                # add newlines between term and definition
+                                if pattern or pattern1:
                                     lines[i] += "\n"
                             
                             with open(output + folder_name + "/" + file_name + ".txt", "w") as txt: # write to .txt files
@@ -39,6 +44,8 @@ def md_to_txt():
     return None
 
 def main():
-    md_to_txt()
+    x = os.path.expanduser("~") + "/Obsidian/brainTwo/"
+    y = os.path.expanduser("~") + "/Downloads/Obsidian/"
+    md_to_txt(x, y)
 
 main()
