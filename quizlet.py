c841635931a0e12d5577aa88bac83b2fe5a1b113
Tomas Gutierrez <tgut03@gmail.com>
2025-10-25 20:00:01 -0500

    create function templates for learning flashcards, multiple choice, write, and test

--- quizlet.py
+++ quizlet.py
@@ -43,17 +43,16 @@ def md_to_txt():
                         # read from .txt files
                         with open(output + folder_name + "/" + file_name + ".txt", "r") as txt:
                             lines = txt.readlines()
-                            len_lines = len(lines[:-2])
 
                             # loop through lines inside .txt file
-                            for i in range(len_lines):
+                            for i in range(len(lines)-2):
                                 pattern = not lines[i].startswith("- ") and lines[i+1].startswith("- ")
                                 pattern1 = lines[i+1].startswith("$$") and not lines[i+2].startswith("---")
                                 pattern2 = lines[i+1].startswith("![[")
 
                                 # add newlines between term and definition
-                                if pattern or pattern1:
-                                    lines[i] += "\n"
+                                # if pattern or pattern1:
+                                #     lines[i] += "\n"
                                 
                                 # delete embedded image link
                                 if pattern2:
@@ -63,6 +62,51 @@ def md_to_txt():
                             with open(output + folder_name + "/" + file_name + ".txt", "w") as txt:
                                 txt.writelines(lines)
     return dirs
+
+
+def clean_data(file_path):
+    """
+    Convert the terms and definitions of obsidian notes into key value pairs
+
+    Parameters
+    ----------
+    file_path : str
+        Location of clicked subdirectory
+
+    Returns
+    -------
+    data : dict
+        Dictionary whose keys are terms and values are definitions
+    """
+    # initialize empty dictionary
+    # open file
+    # read file
+    # loop through lines inside .txt file
+    # pattern match into dictionary
+        # if next line equal "---" then add key into dictionary
+        # if previous line equal "---" then add value into dictionary while line not equal "---"
+    # return dictionary
+
+    with open(file_path, "r") as f:
+        lines = f.readlines()
+        lines.extend(["\n"])
+
+    data = {}
+    for i in range(len(lines)-2):
+        pattern = not lines[i].startswith("- ") and lines[i+1].startswith("- ")
+        pattern1 = not lines[i].startswith("$$") and lines[i+1].startswith("$$") and not lines[i+2].startswith("---")
+        if pattern or pattern1:
+            data[lines[i]] = ""
+        elif lines[i].startswith("---"):
+            continue
+        else:
+            if data:
+                key = list(data)[-1]
+            else:
+                key = list(data)[0]
+            data[key] += lines[i]
+    return data
+
 
 def main():
     md_to_txt()
