239581ca42ee5071244088d0b6ef4f89f6797eae
Tomas Gutierrez <tgut03@gmail.com>
2026-02-03 21:10:21 -0600

    delete clean_data function

--- quizlet.py
+++ quizlet.py
@@ -3,11 +3,6 @@ import os
 def md_to_txt():
     """
     Convert markdown files to text files
-    
-    Returns
-    -------
-    dirs : dict
-        Nested dictionary where the 1st key is the folder_name, the 1st value is a dictionary of file_names, and the 2nd value is the file_path
     """
     input = os.path.expanduser("~") + "/Obsidian/brainTwo/"
     output = os.path.expanduser("~") + "/Github/quizlet/obsidian/"
@@ -52,49 +47,6 @@ def md_to_txt():
                             # write to .txt files
                             with open(output + folder_name + "/" + file_name + ".txt", "w") as txt:
                                 txt.writelines(lines)
-    return dirs
-
-def clean_data(file_path):
-    """
-    Convert the terms and definitions of obsidian notes into key value pairs
-
-    Parameters
-    ----------
-    file_path : str
-        Location of clicked subdirectory
-
-    Returns
-    -------
-    data : dict
-        Dictionary whose keys are terms and values are definitions
-    """
-    with open(file_path, "r") as f:
-        lines = f.readlines()
-        lines.extend(["\n"])
-
-    data = {}
-    # loop through lines of inside .txt file
-    for i in range(len(lines)-2):
-        pattern = not lines[i].startswith("- ") and lines[i+1].startswith("- ")
-        pattern1 = not lines[i].startswith("$$") and lines[i+1].startswith("$$") and not lines[i+2].startswith("---")
-
-        if lines[i].startswith("---"):
-            continue
-        elif lines[i].startswith("![["):
-            continue
-        
-        # term
-        elif pattern or pattern1:
-            data[lines[i]] = ""
-        
-        # definition
-        else:
-            if data:
-                key = list(data)[-1]
-            else:
-                key = list(data)[0]
-            data[key] += lines[i]
-    return data
 
 def main():
     md_to_txt()
