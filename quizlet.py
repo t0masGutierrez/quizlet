c082c265baee5451e425eec01ba76e2aa73559cd
Tomas Gutierrez <tgut03@gmail.com>
2026-03-12 23:09:52 -0500

    format markdown files

--- quizlet.py
+++ quizlet.py
@@ -1,8 +1,8 @@
 import os
 
-def md_to_txt():
+def format():
     """
-    Convert markdown files to text files
+    Format markdown files
     """
     input = os.path.expanduser("~") + "/Obsidian/brainTwo/"
     output = os.path.expanduser("~") + "/Github/quizlet/obsidian/"
@@ -22,34 +22,45 @@ def md_to_txt():
                     file_name = os.path.basename(file)[:-3] # remove suffix ".md"
                     dirs[folder_name][file_name] = file_path
 
-                    # read from .md files
-                    with open(file_path, "r") as md:
-                        data = md.read()
+                    # read from obsidian files
+                    with open(file_path, "r") as obs:
+                        data = obs.read()
                         os.makedirs(output + folder_name + "/", exist_ok=True)
 
-                        # write to .txt files
-                        with open(output + folder_name + "/" + file_name + ".txt", "w") as txt:
-                            txt.write(data)
+                        # write to github files
+                        with open(output + folder_name + "/" + file_name + ".md", "w") as git:
+                            git.write(data)
 
-                        # read from .txt files
-                        with open(output + folder_name + "/" + file_name + ".txt", "r") as txt:
-                            lines = txt.readlines()
+                        # read from github files
+                        with open(output + folder_name + "/" + file_name + ".md", "r") as git:
+                            lines = git.readlines()
 
-                            # loop through lines inside .txt file
+                            # loop through lines inside github file
                             for i in range(len(lines)-2):
-                                pattern = not lines[i].startswith("- ") and lines[i+1].startswith("- ")
+                                pattern = not lines[i].startswith("- ") and lines[i+1].startswith("- ") 
                                 pattern1 = lines[i+1].startswith("$$") and not lines[i+2].startswith("---")
+                                pattern2 = lines[i].startswith("$$") and lines[i+1].startswith("---") 
+                                pattern3 = lines[i].startswith("\\")
 
-                                # add newlines between term and definition
+                                # add hashtags before terms
                                 if pattern or pattern1:
+                                    lines[i] = "### " + lines[i]
+                                    lines[i+1]
+
+                                # add newlines between terms
+                                if pattern1 or pattern2:
                                     lines[i] += "\n"
+                                
+                                # replace alignment
+                                if pattern3:
+                                    lines[i] = lines[i].replace("align*", "aligned")
                             
-                            # write to .txt files
-                            with open(output + folder_name + "/" + file_name + ".txt", "w") as txt:
-                                txt.writelines(lines)
+                            # write to github files
+                            with open(output + folder_name + "/" + file_name + ".md", "w") as git:
+                                git.writelines(lines)
 
 def main():
-    md_to_txt()
+    format()
 
 if __name__ == "__main__":
     main()
